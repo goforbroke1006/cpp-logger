69e1cb23dcd7bfec46f17560bc3eddc47f0c83bb
goforbroke <go.for.broke1006@gmail.com>
2020-10-12 04:12:45 +0300

    Add windows (mingw, vc) compatibility

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Logger for C++ >=11 std project
 
 Single header - **logger.h**
 
-It was checked in Linux for now. Windows coming soon
+### Compilers compatibility
+
+* g++ 9.3.0 (Ubuntu 20) - *OK*
+* cygwin 3.1.7 (Windows 10) - *OK*
+* MinGW 5.4 (Windows 10) - *OK*
+* VS 16.0 (Visual Studio 2019) (Windows 10) - *OK*
+
 
 ### Functions
 
--- logger.h
+++ logger.h
@@ -8,10 +8,24 @@
 #include <iostream>
 #include <stdexcept>
 
+
+#ifdef _WIN32
+
+#include <Windows.h>
+
+#define COLOR_TEXT_RED      12
+#define COLOR_TEXT_GREEN    10
+#define COLOR_TEXT_YELLOW   14
+#define COLOR_TEXT_MAGENTA  13
+
+#else
+
 #define COLOR_TEXT_RED      31
 #define COLOR_TEXT_GREEN    32
 #define COLOR_TEXT_YELLOW   33
 #define COLOR_TEXT_MAGENTA  35
+
+#endif
 
 class Logger {
 public:
@@ -27,24 +41,47 @@ public:
     }
 
 
-    inline static void print(
+    static void print(
             const int colorCode,
             const std::string &levelLabel,
             char const *function, char const *file, long line,
             const std::string &message
     ) {
-        std::cout << "\033[" << colorCode << "m"
-                  << "[" << levelLabel << "] - " << message
-                  << "\033[0m"
-                  << " at " << file << ":" << line << " " << function
-                  << std::endl;
+#ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, colorCode);
+        std::cout
+                << "[" << levelLabel << "] - " << message;
+
+        SetConsoleTextAttribute(hConsole, 0);
+        std::cout
+                << " at " << file << ":" << line << " " << function
+                << std::endl;
+#else
+        std::cout
+                << "\033[" << colorCode << "m"
+                << "[" << levelLabel << "] - " << message
+                << "\033[0m"
+                << " at " << file << ":" << line << " " << function
+                << std::endl;
+#endif
     }
 };
 
-#define Logger_Info(message)        Logger::print(COLOR_TEXT_GREEN, "INFO", __PRETTY_FUNCTION__, __FILE__, __LINE__, (message));
-#define Logger_Warn(message)        Logger::print(COLOR_TEXT_YELLOW, "WARNING", __PRETTY_FUNCTION__, __FILE__, __LINE__, (message));
-#define Logger_Error(message)       Logger::print(COLOR_TEXT_MAGENTA, "ERROR", __PRETTY_FUNCTION__, __FILE__, __LINE__, (message));
-#define Logger_Critical(message)    Logger::critical(__PRETTY_FUNCTION__, __FILE__, __LINE__, (message));
-#define Logger_Fatal(message)       Logger::fatal(__PRETTY_FUNCTION__, __FILE__, __LINE__, (message));
+#if defined(__PRETTY_FUNCTION__)
+
+#define LOGGER_FUNC_NAME __PRETTY_FUNCTION__
+
+#else
+
+#define LOGGER_FUNC_NAME __FUNCTION__
+
+#endif
+
+#define Logger_Info(message)        Logger::print(COLOR_TEXT_GREEN, "INFO", LOGGER_FUNC_NAME, __FILE__, __LINE__, (message));
+#define Logger_Warn(message)        Logger::print(COLOR_TEXT_YELLOW, "WARNING", LOGGER_FUNC_NAME, __FILE__, __LINE__, (message));
+#define Logger_Error(message)       Logger::print(COLOR_TEXT_MAGENTA, "ERROR", LOGGER_FUNC_NAME, __FILE__, __LINE__, (message));
+#define Logger_Critical(message)    Logger::critical(LOGGER_FUNC_NAME, __FILE__, __LINE__, (message));
+#define Logger_Fatal(message)       Logger::fatal(LOGGER_FUNC_NAME, __FILE__, __LINE__, (message));
 
 #endif //CPP_LOGGER_LOGGER_H
